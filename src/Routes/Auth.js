b5a081c7fb9908df9a889abafc20688c0f4970e2
Park <winkknd@naver.com>
2019-05-29 14:30:37 +0900

    #5.0 Auth Route UI part One

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,18 @@
-export default () => "auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Box = styled.div`
+  ${props => props.theme.whiteBox}
+`;
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+
+  return (
+    <Wrapper>
+      {action === "logIn" ? <Box>Log In</Box> : <Box>Sign Up</Box>}
+    </Wrapper>
+  );
+};
